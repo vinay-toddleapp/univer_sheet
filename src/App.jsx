b05312ee73ea61d3336abacd4f443ede25ee93cf
vinay-toddleapp <vinay.pratapsingh@toddleapp.com>
2024-07-23 12:20:31 +0530

    feat: added control to check different POC examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
 import { useRef, useState } from "react";
-import UniverSheet from "./components/UniverSheet";
+import BasicGrid from "./components/BasicGrid";
 import { DEFAULT_WORKBOOK_DATA } from "./helper/constant";
 
 function App() {
   const [data] = useState(DEFAULT_WORKBOOK_DATA);
   const univerRef = useRef();
+  const demoExamples = [
+    {
+      name: "Basic Grid",
+      value: "basicGrid",
+      component: <BasicGrid ref={univerRef} data={data} />,
+    },
+  ];
+
+  const defaultOption = demoExamples[demoExamples.length - 1].value;
 
   return (
-    <div id="root">
-      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
-        <UniverSheet style={{ flex: 1 }} ref={univerRef} data={data} />
-      </div>
+    <div
+      id="root"
+      style={{
+        padding: "50px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+      }}
+    >
+      <select
+        name="demo"
+        id="demo"
+        defaultValue={defaultOption}
+        style={{ width: "fit-content" }}
+      >
+        {demoExamples.map((demo, index) => {
+          return (
+            <option key={index} value={demo.value}>
+              {demo.name}
+            </option>
+          );
+        })}
+      </select>
+      <BasicGrid style={{ flex: 1 }} ref={univerRef} data={data} />
     </div>
   );
 }
